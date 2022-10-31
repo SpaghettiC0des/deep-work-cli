52eb2e6fb19312b6ee17e294aa6150b83c1e4351
karlmarxlopez <7194216+karlmarxlopez@users.noreply.github.com>
2022-10-31 18:31:24 +0800

    feat: initial block command

--- src/commands/block.ts
+++ src/commands/block.ts
@@ -1,28 +1,35 @@
-import {Command, Flags} from '@oclif/core';
-
+import {Command} from '@oclif/core';
+import {isUrlValid} from '../utils';
+import * as fs from 'node:fs';
+import * as sudo from '@vscode/sudo-prompt';
 export default class Block extends Command {
-  static description = 'describe the command here'
+  private hostFilePath = '/etc/hosts'
+  static description = 'adds a website to the block list'
 
   static examples = [
-    '<%= config.bin %> <%= command.id %>',
+    '<%= config.bin %> <%= command.id %> www.google.com',
   ]
 
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  static args = [{name: 'url', required: true}]
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Block);
+    const {args} = await this.parse(Block);
 
-    const name = flags.name ?? 'world';
-    this.log(`hello ${name} from /Users/karlmarxlopez/codes/open-source/deep-work-cli/src/commands/block.ts`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    if (!isUrlValid(args.url)) {
+      this.error(`invalid URL: ${args.url}.`, {exit: 100});
     }
+
+    const hosts = String(await fs.promises.readFile(this.hostFilePath));
+    const firstLine = `0.0.0.0 ${args.url}`;
+    const secondLine = `:: ${args.url}`;
+
+    for (const str of [firstLine, secondLine]) {
+      hosts.replace(new RegExp(`#?[ ]?${str}`, 'g'), '');
+    }
+
+    await fs.promises.writeFile(this.hostFilePath, `${hosts}\n${firstLine}\n${secondLine}`);
+    sudo.exec('dscacheutil -flushcache; sudo killall -HUP mDNSResponder', {name: 'deep-work-cli'});
+
+    this.log(`${args.url} added to the block list!`);
   }
 }
