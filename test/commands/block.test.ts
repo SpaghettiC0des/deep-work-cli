06a9ad4b5eeda7c0c3d219632d4a42005bf11097
karlmarxlopez <7194216+karlmarxlopez@users.noreply.github.com>
2022-10-31 18:30:19 +0800

    test: block command

--- test/commands/block.test.ts
+++ test/commands/block.test.ts
@@ -1,17 +1,31 @@
-import {expect, test} from '@oclif/test'
+import {expect, test} from '@oclif/test';
+import * as fs from 'node:fs';
+
+const FB_URL = 'https://www.facebook.com';
 
 describe('block', () => {
   test
-  .stdout()
+  .stub(fs, 'promises', () => ({readFile: async () => '', writeFile: async () => true}))
+  .stderr()
   .command(['block'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+  .exit(2)
+  .it('should require the website URL');
 
   test
+  .stub(fs, 'promises', () => ({readFile: async () => '', writeFile: async () => true}))
   .stdout()
-  .command(['block', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+  .command(['block', FB_URL])
+  .command(['block', FB_URL.replace('https://', '')])
+  .it('should accept a valid URL', ctx => {
+    expect(ctx.stdout).to.contain(`${FB_URL} added to the block list!`);
+  });
+
+  test
+  .stub(fs, 'promises', () => ({readFile: async () => '', writeFile: async () => true}))
+  .stderr()
+  .stdout()
+  .command(['block', 'invalid url .com'])
+  .exit(100)
+  .command(['block', 'www. reddit.com'])
+  .it('exits with status 100 if the url is invalid');
+});
